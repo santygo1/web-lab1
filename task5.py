d6c419365f388fdaa5cd1f82445a7d2977e2a13d
Danil <danilkaspirin@gmail.com>
2023-11-01 13:56:33 +1000

    fix(task5): remove inner query from SELECt

--- task5.py
+++ task5.py
@@ -11,18 +11,18 @@ from taskexecutor import execute
 query = """
     WITH get_book_max_delay AS ( 
         SELECT 
-            monthno,
-            julianday(lead(borrow_date, 1 ) OVER(PARTITION BY monthno ORDER BY borrow_date)) - julianday(borrow_date) as count
-        FROM (SELECT * , strftime('%m', borrow_date) as monthno FROM book_reader)
+            borrow_date,
+            julianday(lead(borrow_date, 1 ) OVER(PARTITION BY strftime('%m', borrow_date) ORDER BY borrow_date)) - julianday(borrow_date) as count
+        FROM  book_reader
         )
         
     SELECT
-        monthno as Месяц,
-        count(monthno) as Количество,
+        strftime('%m', borrow_date) as Месяц,
+        count(borrow_date) as Количество,
         max(ifnull(count, 0)) as Максимальный_перерыв
     FROM get_book_max_delay
-    GROUP BY monthno
-    ORDER BY monthno
+    GROUP BY Месяц
+    ORDER BY Месяц
 """
 
 execute(query)
